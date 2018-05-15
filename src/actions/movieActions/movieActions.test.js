53e70beae63b174fdb1933e831a68630230dc39d
meloncatty <k.handel66@gmail.com>
2018-05-15 17:31:58 -0600

    Add first action for movieActions

---
+++ src/actions/movieActions/movieActions.test.js
@@ -0,0 +1,14 @@
+import * as actions from './movieActions'
+
+describe("Actions", () => {
+  describe("captureMovies", () => {
+    it("return an action of type CAPTURE_MOVIES", () => {
+      let movies = [{}, {}, {}]
+      const expected = {
+        type: 'CAPTURE_MOVIES',
+        movies: movies
+      }
+      expect(actions.captureMovies(movies)).toEqual(expected)
+    });
+  });
+});
--- src/components/stateful/app/App.test.js
+++ src/components/stateful/app/App.test.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+
+describe("App", () => {
+  it("should be true", () => {
+    expect(true).toEqual(true)
+  });
+});
