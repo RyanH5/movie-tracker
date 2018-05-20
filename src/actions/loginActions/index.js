aac1f9cc2fd0114c8713027d5a6757ab2e2b0523
meloncatty <k.handel66@gmail.com>
2018-05-19 19:16:14 -0600

    Fixing user login

--- src/actions/loginActions/index.js
+++ src/actions/loginActions/index.js
@@ -17,11 +17,23 @@ export const fetchDatabase = (url, email, password) => {
   return async (dispatch) => {
     try {
       dispatch(fetchIsLoading(true));
-      const response = await fetch(url);
+      const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(
+        {
+          password,
+          email
+        }
+      ),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
       if (!response.ok) {
         throw Error(response.statusText);
       }
       dispatch(fetchIsLoading(false));
+      debugger;
       const userData = await response.json();
       const user = findUser(userData, email, password);
       dispatch(userFetchSuccess(user));
