e217974db185ad5ca058b4cfc163473382dbc5fe
Camilo Snapp <camilo.snapp@gmail.com>
2018-05-20 12:13:49 -0600

    Added Header CSS and font

---
+++ src/components/stateless/Favorites/Favorites.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Favorites extends Component {
+  render () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+Favorites.propTypes = {};
+
+export default Favorites;
--- src/components/stateless/app/App.js
+++ src/components/stateless/app/App.js
@@ -7,21 +7,25 @@ import PropTypes from 'prop-types';
 import CreateUser from '../../stateful/createUser/CreateUser';
 import LoginUser from '../../stateful/loginUser/LoginUser';
 import MovieContainer from '../../stateful/movieContainer/MovieContainer';
+import Favorites from '../Favorites/Favorites'
 import './styles.css';
 
 export class App extends Component {
   render () {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header d-flex flex-justify-between">
           <h1 className="App-title">Movie Tracker</h1>
-          <NavLink to="/" className="nav">Home</NavLink>
-          <NavLink to="/login" className="nav">Login</NavLink>
-          <NavLink to="/createUser" className="nav">Create User</NavLink>
+            <nav>
+              <li><NavLink to="/login" className="login">Login</NavLink></li>
+              <li><NavLink to="/createUser" className="create-user">Create User</NavLink></li>
+              <li><NavLink to="/favorites" className="favorites">Favorites</NavLink></li>
+            </nav>
         </header>
         <Route exact path='/' component={MovieContainer} />
         <Route exact path='/login' component={LoginUser} />
         <Route exact path='/createUser' component={CreateUser} />
+        <Route exact path='/favorites' component={Favorites} />
       </div>
     );
   };
