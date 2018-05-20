e14ea5979cb95d39ee005ff31f196f58bae8965b
Camilo Snapp <camilo.snapp@gmail.com>
2018-05-20 13:31:56 -0600

    Added CSS for header, added logo and some body CSS

--- src/components/stateless/app/App.js
+++ src/components/stateless/app/App.js
@@ -9,18 +9,21 @@ import LoginUser from '../../stateful/loginUser/LoginUser';
 import MovieContainer from '../../stateful/movieContainer/MovieContainer';
 import Favorites from '../Favorites/Favorites'
 import './styles.css';
+import logo from '../../../assets/MM-logo.png';
 
 export class App extends Component {
   render () {
     return (
       <div className="App">
-        <header className="App-header d-flex flex-justify-between">
-          <h1 className="App-title">Movie Tracker</h1>
-            <nav>
-              <li><NavLink to="/login" className="login">Login</NavLink></li>
-              <li><NavLink to="/createUser" className="create-user">Create User</NavLink></li>
-              <li><NavLink to="/favorites" className="favorites">Favorites</NavLink></li>
-            </nav>
+        <header className="App-header  ">
+          <div className="inner-col d-flex flex-justify-between">
+            <img src={logo} sizes="100vw" alt="Movie Tracker Logo" />
+              <nav>
+                <li><NavLink to="/login" className="login">Login</NavLink></li>
+                <li><NavLink to="/createUser" className="create-user">Create User</NavLink></li>
+                <li><NavLink to="/favorites" className="favorites">Favorites</NavLink></li>
+              </nav>
+          </div>
         </header>
         <Route exact path='/' component={MovieContainer} />
         <Route exact path='/login' component={LoginUser} />
