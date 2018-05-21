8df7a9b5a7ba98359c681b905a84cc7607b3e439
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-05-21 12:43:58 -0600

    Alter userFetchSuccess parameter to reflect change from user to userId

--- src/actions/loginActions/index.test.js
+++ src/actions/loginActions/index.test.js
@@ -1,46 +1,47 @@
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import {fetchIsLoading,
-        fetchErrored,
-        userFetchSuccess,
-        fetchDatabase,
-        findUser} from './index'
+  fetchErrored,
+  userFetchSuccess,
+  fetchDatabase,
+  findUser} from './index';
 
 describe("Login Actions", () => {
   describe("fetchIsLoading", () => {
     it("should return type FETCH_LOADING", () => {
-      const bool = false
+      const bool = false;
       const expected = {
         type: 'FETCH_LOADING',
         isLoading: bool
-      }
-      expect(fetchIsLoading(bool)).toEqual(expected)
+      };
+      expect(fetchIsLoading(bool)).toEqual(expected);
     });
   });
 
   describe("fetchErrored", () => {
     it("should return type FETCH_ERRORED", () => {
-      const bool = false
+      const bool = false;
       const expected = {
         type: 'FETCH_ERRORED',
         isErrored: bool
-      }
-      expect(fetchErrored(bool)).toEqual(expected)
+      };
+      expect(fetchErrored(bool)).toEqual(expected);
     });
   });
 
   describe("userFetchSuccess", () => {
     it("should return type USER_FETCH_SUCCESS", () => {
-      const user = {
-        email: 'howiedewitt@aol.com',
-        password: 'pass'
-      }
+      // const user = {
+      //   email: 'howiedewitt@aol.com',
+      //   password: 'pass'
+      // };
+      const userId = {userId: 3};
       const expected = {
         type: 'USER_FETCH_SUCCESS',
-        user
-      }
-      expect(userFetchSuccess(user)).toEqual(expected)
+        userId
+      };
+      expect(userFetchSuccess(userId)).toEqual(expected);
     });
   });
 });
@@ -51,25 +52,25 @@ describe("fetchDatabase", () => {
   let mockPassword;
   let mockData;
   beforeEach(() => {
-    mockUrl = 'fake.url'
-    mockEmail = 'fakemail@gmail.com'
-    mockPassword = 'pass'
+    mockUrl = 'fake.url';
+    mockEmail = 'fakemail@gmail.com';
+    mockPassword = 'pass';
     mockData = {
       id: 'userID',
       email: mockEmail,
       password: mockPassword,
       name: 'name'
-    }
+    };
     window.fetch = jest.fn().mockImplementation(() => {
       return Promise.resolve({
         status: 200,
         json: () => Promise.resolve(mockData)
-      })
+      });
     });
-  })
+  });
   it("should call fetch with correct parameters", () => {
-    fetchDatabase(mockUrl, mockEmail, mockPassword)
-    expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+    fetchDatabase(mockUrl, mockEmail, mockPassword);
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl);
   });
 
   it("should call fetchErrored if status is not okay", () => {
