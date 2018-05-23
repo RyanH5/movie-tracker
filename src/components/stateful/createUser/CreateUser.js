c2fcdfe46a04275499f0bed56cd454606d501e2f
meloncatty <k.handel66@gmail.com>
2018-05-22 21:37:55 -0600

    Remove merge comments

--- src/components/stateful/createUser/CreateUser.js
+++ src/components/stateful/createUser/CreateUser.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { createNewUser } from '../../../actions/createUserActions/index.js';
-import { fetchDatabase } from '../../../actions/loginActions/index.js'
-=======
 import { createNewUser, userCreateSuccess } from '../../../actions/createUserActions';
->>>>>>> 0d1b2e1b725dfd6a2ddba64b3f8bd1330f131f68
 import { connect } from 'react-redux';
 import './CreateUser.css';
 
@@ -26,17 +21,10 @@ class CreateUser extends Component {
   handleSubmit = async (event) => {
     event.preventDefault();
     const {name, email, password} = this.state;
-<<<<<<< HEAD
-    if(this.props.invalidForm && this.props.createAccountFailed) {
-      this.props.createNewUser(name, email, password);
-      const url = 'http://localhost:3000/api/users';
-      this.props.fetchDatabase(url, email, password)
-=======
     const url = 'http://localhost:3000/api/users/new';
     await this.props.createNewUser(url, name, email, password);
     if (this.props.loginSuccess &&
       !this.props.invalidForm) {
->>>>>>> 0d1b2e1b725dfd6a2ddba64b3f8bd1330f131f68
       this.props.history.push('/')
     }
   };
@@ -100,12 +88,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-<<<<<<< HEAD
-  createNewUser: (name, email, password) => dispatch(createNewUser(name, email, password)),
-  fetchDatabase: (url, email, password) => dispatch(fetchDatabase(url, email, password))
-=======
   createNewUser: (url, name, email, password) => dispatch(createNewUser(url, name, email, password))
->>>>>>> 0d1b2e1b725dfd6a2ddba64b3f8bd1330f131f68
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateUser);
